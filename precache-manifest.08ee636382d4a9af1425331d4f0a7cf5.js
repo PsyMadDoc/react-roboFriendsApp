7d33314c412c1cff926fc370f0bb2d24c3fc0463
PsyMadDoc <v.d.cruceanu@gmail.com>
2020-05-14 00:16:30 +0300

    Updates

---
+++ precache-manifest.08ee636382d4a9af1425331d4f0a7cf5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f42a0ac9eedd4266ae057cc344df84",
+    "url": "/roboFriendsApp/index.html"
+  },
+  {
+    "revision": "3dbaac5a522c343a46eb",
+    "url": "/roboFriendsApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1462fc8b4d9e6d937fb9",
+    "url": "/roboFriendsApp/static/css/main.0c8627dc.chunk.css"
+  },
+  {
+    "revision": "3dbaac5a522c343a46eb",
+    "url": "/roboFriendsApp/static/js/2.d82cd422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboFriendsApp/static/js/2.d82cd422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1462fc8b4d9e6d937fb9",
+    "url": "/roboFriendsApp/static/js/main.7c62736a.chunk.js"
+  },
+  {
+    "revision": "ada622f8557fbfecb688",
+    "url": "/roboFriendsApp/static/js/runtime-main.cdcee2ca.js"
+  },
+  {
+    "revision": "6cb73b0e7f6275e187ab1fbd953d8200",
+    "url": "/roboFriendsApp/static/media/RouleauxFilledLight.6cb73b0e.woff"
+  }
+]);
